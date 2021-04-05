6d952fb2e508ded0669d66aec624979e1a64978e
Kelvin O'Shea <kelvinmoshea@gmail.com>
2021-04-05 17:05:59 +1000

    add favicon

--- pages/index.js
+++ pages/index.js
@@ -7,12 +7,15 @@ export default function Home() {
     <div className={styles.container}>
       <Head>
         <title>Kelvin O'Shea - Product Designer</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+        <link rel="manifest" href="/site.webmanifest"></link>
         <meta name="description" content="Kelvin O'Shea - Product Design @ Clipchamp"/>
-          <meta name="author" content="Kelvin O'Shea"/>
-          <meta http-equiv='X-UA-Compatible' content='IE=edge'/>
-          <meta http-equiv='content-type' content='text/html; charset=utf-8'/>
-	        <meta name='viewport' content='width=device-width, initial-scale=1.0, minimal-ui, user-scalable=0'/>
+        <meta name="author" content="Kelvin O'Shea"/>
+        <meta http-equiv='X-UA-Compatible' content='IE=edge'/>
+        <meta http-equiv='content-type' content='text/html; charset=utf-8'/>
+	      <meta name='viewport' content='width=device-width, initial-scale=1.0, minimal-ui, user-scalable=0'/>
       </Head>
 
       <main className={styles.main}>
